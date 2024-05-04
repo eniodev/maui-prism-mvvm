067f21b259893c4124b3819e09378bd32c393ce0
eniodev <enioblvck@gmail.com>
2024-05-04 19:37:24 +0100

    initial setup

---
+++ MAUi/MAUi/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUi;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ MAUi/MAUi/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUi.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
+
